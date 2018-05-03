ffff8e31bb69f4bcc89dfaaed9c66a23c521ec14
KarolBurgos <karburgos@umariana.edu.co>
2018-05-03 11:43:59 -0500

    Proyecto protegems

--- app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/fragmenttabbed/InicioFragment.java
+++ app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/fragmenttabbed/InicioFragment.java
@@ -14,6 +14,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,6 +27,7 @@ import android.widget.AdapterViewFlipper;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -33,6 +36,7 @@ import com.example.co.com.revistaprotegemos.appprotegemos.Banner.CustomAdapter;
 import com.example.co.com.revistaprotegemos.appprotegemos.PrincipalFragment;
 import com.example.co.com.revistaprotegemos.appprotegemos.R;
 import com.example.co.com.revistaprotegemos.appprotegemos.Suscribete.SuscribeteActivity;
+import com.example.co.com.revistaprotegemos.appprotegemos.ViewPagerAdapter;
 import com.example.co.com.revistaprotegemos.appprotegemos.WebViewAbrirPaginasUrl;
 import com.example.co.com.revistaprotegemos.appprotegemos.settings.NuestraEmpresaActivity;
 import com.example.co.com.revistaprotegemos.appprotegemos.validacionnohayinternet.ValidacionNoHayInternet;
@@ -53,6 +57,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import pl.droidsonroids.gif.GifImageView;
 import retrofit2.Call;
@@ -66,6 +72,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * A simple {@link Fragment} subclass.
  */
 public class InicioFragment extends Fragment {
+
+    //Banner protegemos
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private  int dotscount;
+    //B
+
+    private ImageView[] dots;
     private RecyclerView recyclerView;
     private RecyclerView recyclerView2;
     private ArrayList<Pautas> data;
@@ -100,6 +114,51 @@ public class InicioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        //Banner protegemos
+        viewPager=(ViewPager)view.findViewById(R.id.viewPager);
+        sliderDotspanel =(LinearLayout)view.findViewById(R.id.SliderDots);
+        ViewPagerAdapter viewPagerAdapter =new ViewPagerAdapter(getContext());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        dotscount =viewPagerAdapter.getCount();
+        dots =new ImageView[dotscount];
+        for (int i=0;i<dotscount; i++)
+        {
+            dots[i]=new ImageView(getContext());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.nonactive_dot));
+            LinearLayout.LayoutParams params =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8,0,8,0);
+            sliderDotspanel.addView(dots[i],params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i=0;i<dotscount;i++)
+                {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),2000,4000);
+
+        //Fin Banner protegemos
+
 
         facebook=(ImageButton)view.findViewById(R.id.facebook);
         twitter=(ImageButton)view.findViewById(R.id.twitter);
@@ -169,10 +228,11 @@ public class InicioFragment extends Fragment {
         bsuscr=(Button)view.findViewById(R.id.btonsuscribirme);
 
 
-        IVF = (AdapterViewFlipper) view.findViewById(R.id.IVF);
+        //Bannerconmovimiento
+/*        IVF = (AdapterViewFlipper) view.findViewById(R.id.IVF);
         CustomAdapter custom = new CustomAdapter(myContext.getApplicationContext(), names, images);
         IVF.setAdapter(custom);
-        IVF.setFlipInterval(2000);
+        IVF.setFlipInterval(2000);*/
         //IVF.setAutoStart(true);
 
         GifImageView givImageView = (GifImageView) view.findViewById(R.id.iges1);
@@ -375,6 +435,29 @@ public class InicioFragment extends Fragment {
             }
         });
 
+    }
+
+    public class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+           /* getContext().this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+
+                }
+            });*/
+
+        }
     }
 
 }
