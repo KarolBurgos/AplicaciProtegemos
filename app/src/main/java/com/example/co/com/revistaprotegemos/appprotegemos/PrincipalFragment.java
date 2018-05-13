af8cb9a461a5d8258e5774f3131a5f078190850c
Karol Burgos <karburgos@umariana.edu.co>
2018-05-13 18:34:48 -0500

    Proyecto protegems

--- app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/PrincipalFragment.java
+++ app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/PrincipalFragment.java
@@ -200,7 +200,7 @@ public class PrincipalFragment extends Fragment {
             switch (position) {
                 case 0:
 
-                    return new InicioFragment();
+                    return new InicioAppFragment();
 
                 case 1:
                     return new PlanesFragment();
