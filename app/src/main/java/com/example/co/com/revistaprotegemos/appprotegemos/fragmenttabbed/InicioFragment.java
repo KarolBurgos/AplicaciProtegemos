3712c0935d8bb7db968fd954e121f5dd963a1c8c
KarolBurgos <karburgos@umariana.edu.co>
2018-05-17 23:25:42 -0500

    Proyecto protegems

--- app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/fragmenttabbed/InicioFragment.java
+++ app/src/main/java/com/example/co/com/revistaprotegemos/appprotegemos/fragmenttabbed/InicioFragment.java
@@ -77,36 +77,32 @@ public class InicioFragment extends Fragment {
     //Banner protegemos
     ViewPager viewPager;
     LinearLayout sliderDotspanel;
-    private  int dotscount;
+    private int dotscount;
     //B
 
     private ImageView[] dots;
-    private RecyclerView recyclerView;
-    private RecyclerView recyclerView2;
-    private ArrayList<Pautas> data;
-    private ArrayList<Jornadas> data2;
-    private DataAdapterr adapter;
-    private DataaAdapter adapter2;
-    private ImageView imagen1,img2;
-    private TextView t1,t2;
+    private RecyclerView recyclerViewPautas;
+    private RecyclerView recyclerViewJornadas;
+    private ArrayList<Pautas> dataPautas;
+    private ArrayList<Jornadas> dataJornadas;
+    private DataAdapterr adapterPautas;
+    private DataaAdapter adapterJornadas;
     private FragmentActivity myContext;
-    private Button bsuscr,btn;
-    private AdapterViewFlipper IVF;
-    private String url;
-    private TextView quienes_somos,visite,encontrara,jornada;
-    private TextView tg1,nuestrosplanes,susc,somo,estaslisto,pautas;
-    private Typeface Nuestrosplanes,Aurella;
-    private ImageButton facebook,twitter;
+    private Button bsuscr, btn;
+    //Botones de facebook y twitter
+    private ImageButton facebook, twitter;
+
+    //Atributo para actualizar la pagina
     SwipeRefreshLayout swipeRefreshLayout;
-    TextView textView;
-    int number=0;
+
+
     public InicioFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onAttach(Context activity) {
-        myContext=(FragmentActivity) activity;
+        myContext = (FragmentActivity) activity;
         super.onAttach(activity);
     }
 
@@ -116,54 +112,11 @@ public class InicioFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
 
-        //Banner protegemos
-/*        viewPager=(ViewPager)view.findViewById(R.id.viewPager);
-        sliderDotspanel =(LinearLayout)view.findViewById(R.id.SliderDots);
-        ViewPagerAdapter viewPagerAdapter =new ViewPagerAdapter(getContext());
-        viewPager.setAdapter(viewPagerAdapter);
+        facebook = (ImageButton) view.findViewById(R.id.facebook);
+        twitter = (ImageButton) view.findViewById(R.id.twitter);
 
-        dotscount =viewPagerAdapter.getCount();
-        dots =new ImageView[dotscount];
-        for (int i=0;i<dotscount; i++)
-        {
-            dots[i]=new ImageView(getContext());
-            dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.nonactive_dot));
-            LinearLayout.LayoutParams params =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(8,0,8,0);
-            sliderDotspanel.addView(dots[i],params);
-        }
-        dots[0].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.active_dot));
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                for(int i=0;i<dotscount;i++)
-                {
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.nonactive_dot));
-                }
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getContext(),R.drawable.active_dot));
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        Timer timer=new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(),2000,4000);*/
-
-        //Fin Banner protegemos
-
-
-        facebook=(ImageButton)view.findViewById(R.id.facebook);
-        twitter=(ImageButton)view.findViewById(R.id.twitter);
-        swipeRefreshLayout=(SwipeRefreshLayout)view.findViewById(R.id.Swipe);
+        //Permite actualizar la pagina
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.Swipe);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -172,13 +125,13 @@ public class InicioFragment extends Fragment {
 
                     // Load Webview
                     Fragment fragment = null;
-                    Class fragmentClass= PrincipalFragment.class;
-                    try{
+                    Class fragmentClass = PrincipalFragment.class;
+                    try {
                         fragment = (Fragment) fragmentClass.newInstance();
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    FragmentManager fragmentManager=myContext.getSupportFragmentManager();
+                    FragmentManager fragmentManager = myContext.getSupportFragmentManager();
                     fragmentManager.beginTransaction().replace(R.id.flContentt, fragment).commit();
 
                 } else {
@@ -194,78 +147,38 @@ public class InicioFragment extends Fragment {
                     public void run() {
                         swipeRefreshLayout.setRefreshing(false);
                     }
-                },4000);
+                }, 4000);
             }
         });
 
-            recyclerView = (RecyclerView) view.findViewById(R.id.recyclerVieww);
-            recyclerView.setHasFixedSize(true);
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
-            recyclerView.setLayoutManager(layoutManager);
-            loadJSONn();
+        //Listado de pautas
+        recyclerViewPautas = (RecyclerView) view.findViewById(R.id.recyclerVieww);
+        recyclerViewPautas.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewPautas.setLayoutManager(layoutManager);
+        loadJSONPautas();
 
-            recyclerView2 = (RecyclerView) view.findViewById(R.id.recyclerVieew);
-            recyclerView2.setHasFixedSize(true);
-            /*GridLayoutManager gridLayoutManager2 = new GridLayoutManager(getActivity().getApplicationContext(), 2);
-            recyclerView2.setLayoutManager(gridLayoutManager2);*/
-            RecyclerView.LayoutManager layoutManager2 = new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
-            recyclerView2.setLayoutManager(layoutManager2);
-            loadJSON();
-
-
-   /*          Bitmap obtener_imagen = get_imagen("http://www.revistaprotegemos.com.co/imagenesaplicativo/premia.png");
-             //uImageView.setImageBitmap(obtener_imagen);
-            Drawable d = new BitmapDrawable(getResources(), obtener_imagen);
-            int drawableId = Integer.parseInt(d.toString());*/
-
-
-
-
-            int images[] = {R.drawable.premia, R.drawable.drogueria, R.drawable.ips};
-            String names[] = {"imagen1", "imagen2", "imagen3"};
-
-        bsuscr=(Button)view.findViewById(R.id.btonsuscribirme);
-
-
-        //Bannerconmovimiento
-/*        IVF = (AdapterViewFlipper) view.findViewById(R.id.IVF);
-        CustomAdapter custom = new CustomAdapter(myContext.getApplicationContext(), names, images);
-        IVF.setAdapter(custom);
-        IVF.setFlipInterval(5000);*/
-        //IVF.setAutoStart(true);
+        //Listado de jornadas
+        recyclerViewJornadas = (RecyclerView) view.findViewById(R.id.recyclerVieew);
+        recyclerViewJornadas.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager2 = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewJornadas.setLayoutManager(layoutManager2);
+        loadJSONJornadas();
+        bsuscr = (Button) view.findViewById(R.id.btonsuscribirme);
 
         return view;
     }
-    public void inter() {
 
-    }
-    private Bitmap get_imagen(String url) {
-        Bitmap bm = null;
-        try {
-            URL _url = new URL(url);
-            URLConnection con = _url.openConnection();
-            con.connect();
-            InputStream is = con.getInputStream();
-            BufferedInputStream bis = new BufferedInputStream(is);
-            bm = BitmapFactory.decodeStream(bis);
-            bis.close();
-            is.close();
-        } catch (IOException e) {
-
-        }
-        return bm;
-    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 
-
         bsuscr.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent intent=new Intent (getContext(),SuscribeteActivity.class);
+                Intent intent = new Intent(getContext(), SuscribeteActivity.class);
                 startActivity(intent);
 
 
@@ -289,7 +202,8 @@ public class InicioFragment extends Fragment {
         });
     }
 
-    private void loadJSON(){
+    //Metodo para cargar las jPautas , llamand a la interfaz api y al adaptador
+    private void loadJSONPautas() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://181.62.161.60")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -301,20 +215,21 @@ public class InicioFragment extends Fragment {
             public void onResponse(Call<JSONResponsee> call, Response<JSONResponsee> response) {
 
                 JSONResponsee jsonResponse = response.body();
-                data = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
-                adapter = new DataAdapterr(data,getContext());
-                recyclerView.setAdapter(adapter);
+                dataPautas = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
+                adapterPautas = new DataAdapterr(dataPautas, getContext());
+                recyclerViewPautas.setAdapter(adapterPautas);
             }
 
             @Override
             public void onFailure(Call<JSONResponsee> call, Throwable t) {
-                Log.d("Error",t.getMessage());
+                Log.d("Error", t.getMessage());
             }
         });
 
     }
 
-    private void loadJSONn(){
+    //Metodo para cargar Jornadas
+    private void loadJSONJornadas() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://181.62.161.60")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -326,40 +241,17 @@ public class InicioFragment extends Fragment {
             public void onResponse(Call<JSOONResponse> call, Response<JSOONResponse> response) {
 
                 JSOONResponse jsonResponse = response.body();
-                data2 = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
-                adapter2 = new DataaAdapter(data2,getContext());
-                recyclerView2.setAdapter(adapter2);
+                dataJornadas = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
+                adapterJornadas = new DataaAdapter(dataJornadas, getContext());
+                recyclerViewJornadas.setAdapter(adapterJornadas);
             }
 
             @Override
             public void onFailure(Call<JSOONResponse> call, Throwable t) {
-                Log.d("Error",t.getMessage());
+                Log.d("Error", t.getMessage());
             }
         });
 
-    }
-
-    public class MyTimerTask extends TimerTask {
-
-        @Override
-        public void run() {
-
-           /* getContext().this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-
-                    if(viewPager.getCurrentItem() == 0){
-                        viewPager.setCurrentItem(1);
-                    } else if(viewPager.getCurrentItem() == 1){
-                        viewPager.setCurrentItem(2);
-                    } else {
-                        viewPager.setCurrentItem(0);
-                    }
-
-                }
-            });*/
-
-        }
     }
 
 }
